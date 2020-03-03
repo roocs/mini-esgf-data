7623d83313d63db5f2b5da6b846095b6e8f35c42
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-03-03 16:29:06 +0000

    attempt at writing script in python

---
+++ generate_test_data.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+from nco import Nco
+
+
+nco = Nco()
+# generate condensed versions of data to use as test data
+
+Amon_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas"
+
+fpath = Amon_file_path
+filelist = os.listdir(fpath)
+output_path = "cmip5"
+
+for file in filelist:
+    path = f"{fpath}/{file}"
+    var_id = file.split("_")[0]
+    output_file = f"{output_path}/{file}"
+
+    lat_selector = "-d lat,,,100"
+    lon_selector = "-d lon,,,100"
+
+    extra = ""
+
+    #nco.ncks(input=f'{path}', output=f'{output_file}', variable=f'{var_id}', dim=f'{lat_selector}, {lon_selector}')
+
+    print(path, output_file)
+    #
+    cmd = f"ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}"
+    # print("running", cmd)
+    subprocess.call(cmd, shell=True)
