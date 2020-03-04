9e3c303fc32273d7016e206e35a8e4761f879805
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-03-04 09:28:49 +0000

    updated file paths to work with testing prototype libraries

--- generate_test_data.sh
+++ generate_test_data.sh
@@ -2,14 +2,16 @@
 
 # Define all collections of files to copy to "small" local files
 
-Amon_files="/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/tas*.nc"
+Amon_files="/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/tas_*.nc"
 
-output_dir=test_data/cmip5
+
+files=$Amon_files
+#file_path=$(dirname "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/tas_*.nc")
+file_path=$(dirname $(printf $files))
+output_dir=test_data$(echo $file_path)
 mkdir -p $output_dir
 
 # Define files variable as the files to convert this time
-files=$Amon_files
-
 for f in $files ; do
     fname=$(basename $f)
     var_id=$(echo $fname | cut -d_ -f1)
@@ -33,8 +35,8 @@ for f in $files ; do
         lon_selector=""
     fi
 
-    echo $var_id
     cmd="ncks $extra $lat_selector $lon_selector -v $var_id $f $output_file"
+    echo $var_id
     echo "Running: $cmd"
     $cmd
     echo "Wrote: $output_file"
