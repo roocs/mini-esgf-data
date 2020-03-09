36950884d2cbe141d5ec4a98dcf1a6cb47ee594a
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-03-09 10:38:45 +0000

    more test data

--- generate_test_data.py
+++ generate_test_data.py
@@ -7,10 +7,11 @@ nco = Nco()
 # generate condensed versions of data to use as test data
 
 Amon_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas"
+zostoga_file_path = "/badc/cmip5/data/cmip5/output1/INM/inmcm4/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
 
-fpath = Amon_file_path
+fpath = zostoga_file_path
 filelist = os.listdir(fpath)
-output_path = "cmip5"
+output_path = "test_data/cmip5"
 
 for file in filelist:
     path = f"{fpath}/{file}"
@@ -26,6 +27,6 @@ for file in filelist:
 
     print(path, output_file)
     #
-    cmd = f"ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}"
-    # print("running", cmd)
+    cmd = f"ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}".split()
+    print("running", cmd)
     subprocess.call(cmd, shell=True)
--- generate_test_data.sh
+++ generate_test_data.sh
@@ -4,9 +4,11 @@
 
 Amon_files="/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/tas_*.nc"
 zostoga_files="/badc/cmip5/data/cmip5/output1/INM/inmcm4/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga/zostoga_*.nc"
+Lmon_files="/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/*.nc"
+zostoga_2_files="/badc/cmip5/data/cmip5/output1/MPI-M/MPI-ESM-LR/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga/zostoga_*.nc"
 
 #update this for the files being created
-files=$Amon_files
+files=$zostoga_2_files
 #file_path=$(dirname "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/tas_*.nc")
 file_path=$(dirname $(printf $files))
 output_dir=test_data$(echo $file_path)
@@ -34,6 +36,12 @@ for f in $files ; do
 
     if [[ $fname =~ "ssp24" ]]; then
         lon_selector=""
+    fi
+
+    if [[ $fname =~ "zostoga" ]]; then
+        lon_selector=""
+        lat_selector=""
+        extra="-d lev ,,,8"
     fi
 
     cmd="ncks $extra $lat_selector $lon_selector -v $var_id $f $output_file"
