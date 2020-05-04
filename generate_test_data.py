67540ffd6296da24a18a68f0587e284f69741096
Eleanor Smith <esmith88@jasmin-sci4.ceda.ac.uk>
2020-05-04 15:32:01 +0100

    another zostoga example

--- generate_test_data.py
+++ generate_test_data.py
@@ -6,10 +6,10 @@ from glob import glob
 # generate condensed versions of data to use as test data
 
 Amon_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas"
-zostoga_file_path = "/badc/cmip5/data/cmip5/output1/INM/inmcm4/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
+zostoga_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
 cordex_IPSL_file_path = "/group_workspaces/jasmin2/cp4cds1/data/c3s-cordex/output/EUR-11/IPSL/MOHC-HadGEM2-ES/rcp85/r1i1p1/IPSL-WRF381P/v1/day/psl/v20190212"
 
-fpath = cordex_IPSL_file_path
+fpath = zostoga_file_path
 filelist = glob(f'{fpath}/*.nc')
 output_path = f"test_data{fpath}"
 
@@ -36,6 +36,6 @@ for file in filelist:
         extra = ""
 
 
-    cmd = f"ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}"
+    cmd = f"ncks  {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}"
     print("running", cmd)
     subprocess.call(cmd, shell=True)
