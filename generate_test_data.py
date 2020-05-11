5dea9e85eb36c6597db060aafdc6926bf1bdf3bb
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-05-11 10:24:30 +0100

    more zostoga test data

--- generate_test_data.py
+++ generate_test_data.py
@@ -6,7 +6,7 @@ from glob import glob
 # generate condensed versions of data to use as test data
 
 Amon_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas"
-zostoga_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
+zostoga_file_path = "/badc/cmip5/data/cmip5/output1/IPSL/IPSL-CM5A-LR/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
 cordex_IPSL_file_path = "/group_workspaces/jasmin2/cp4cds1/data/c3s-cordex/output/EUR-11/IPSL/MOHC-HadGEM2-ES/rcp85/r1i1p1/IPSL-WRF381P/v1/day/psl/v20190212"
 
 fpath = zostoga_file_path
@@ -28,7 +28,8 @@ for file in filelist:
     if "zostoga" in file:
         lon_selector = ""
         lat_selector = ""
-        extra = "-d lev,,,8"
+        if "inm" in file:
+            extra = "-d lev,,,8"
 
     if "cordex" in file:
         lon_selector = "-d rlat,,,100"
@@ -36,6 +37,6 @@ for file in filelist:
         extra = ""
 
 
-    cmd = f"ncks  {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}"
+    cmd = f"ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}"
     print("running", cmd)
     subprocess.call(cmd, shell=True)
