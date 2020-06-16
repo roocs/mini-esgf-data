d6e7f0d8a0c4d7d43b5f64e17fb4508dd68ad3d0
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-06-16 09:39:24 +0100

    cmip6 siconc test data

--- generate_test_data.py
+++ generate_test_data.py
@@ -9,8 +9,10 @@ Amon_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos
 zostoga_file_path = "/badc/cmip5/data/cmip5/output1/IPSL/IPSL-CM5A-MR/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
 cordex_IPSL_file_path = "/group_workspaces/jasmin2/cp4cds1/data/c3s-cordex/output/EUR-11/IPSL/MOHC-HadGEM2-ES/rcp85/r1i1p1/IPSL-WRF381P/v1/day/psl/v20190212"
 no_time_name = "/group_workspaces/jasmin2/cp4cds1/vol1/data/c3s-cmip5/output1/ICHEC/EC-EARTH/historical/day/atmos/day/r1i1p1/tas/v20131231"
+CMIP6_siconc = "/badc/cmip6/data/CMIP6/CMIP/NOAA-GFDL/GFDL-ESM4/historical/r1i1p1f1/SImon/siconc/gn/latest"
 
-fpath = no_time_name
+
+fpath = CMIP6_siconc
 filelist = glob(f'{fpath}/*.nc')
 output_path = f"test_data{fpath}"
 
@@ -37,6 +39,10 @@ for file in filelist:
         lat_selector = "-d rlon,,,100"
         extra = ""
 
+    if "siconc" in file:
+        lon_selector = ""
+        lat_selector = ""
+        extra = "-d x,,,100 -d y,,,100"
 
     cmd = f"ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {path} {output_file}"
     print("running", cmd)
