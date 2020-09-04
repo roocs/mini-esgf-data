10e6540b98b6e67042d6a3970017442399c2589b
Eleanor Smith <esmith88@jasmin-sci4.ceda.ac.uk>
2020-09-04 14:23:09 +0100

    formatting

--- generate_test_data.py
+++ generate_test_data.py
@@ -9,16 +9,18 @@ Amon_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos
 zostoga_file_path = "/badc/cmip5/data/cmip5/output1/IPSL/IPSL-CM5A-MR/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
 cordex_IPSL_file_path = "/group_workspaces/jasmin2/cp4cds1/data/c3s-cordex/output/EUR-11/IPSL/MOHC-HadGEM2-ES/rcp85/r1i1p1/IPSL-WRF381P/v1/day/psl/v20190212"
 no_time_name = "/group_workspaces/jasmin2/cp4cds1/vol1/data/c3s-cmip5/output1/ICHEC/EC-EARTH/historical/day/atmos/day/r1i1p1/tas/v20131231"
-CMIP6_siconc = "/badc/cmip6/data/CMIP6/CMIP/NCAR/CESM2/historical/r1i1p1f1/SImon/siconc/gn/latest"
+CMIP6_siconc = (
+    "/badc/cmip6/data/CMIP6/CMIP/NCAR/CESM2/historical/r1i1p1f1/SImon/siconc/gn/latest"
+)
 cmip5_tas = "/badc/cmip5/data/cmip5/output1/NCAR/CCSM4/historical/mon/atmos/Amon/r1i1p1/latest/tas"
 
 fpath = cmip5_tas
-filelist = glob(f'{fpath}/*.nc')
+filelist = glob(f"{fpath}/*.nc")
 output_path = f"test_data{fpath}"
 
 for file in filelist:
     path = f"{file}"
-    f = file.split('/')[-1]
+    f = file.split("/")[-1]
     var_id = f.split("_")[0]
     output_file = f"test_data{file}"
     if not os.path.exists(output_file):
