4ec5383eaca47bd95a9e161fa323b30a6e2749de
Eleanor Smith <esmith88@sci5.jasmin.ac.uk>
2020-12-14 15:10:18 +0000

    c3s-cmip6 files

--- generate_test_data.py
+++ generate_test_data.py
@@ -17,8 +17,9 @@ plevs_file_path = "/badc/cmip6/data/CMIP6/CMIP/NOAA-GFDL/GFDL-ESM4/historical/r1
 day_data = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/land/day/r1i1p1/latest/mrsos"
 cmip6_Cl = "/badc/cmip6/data/CMIP6/CMIP/NCAR/CESM2/amip/r1i1p1f1/Amon/cl/gn/v20190319"
 cmip6_Cl_2 = "/badc/cmip6/data/CMIP6/CMIP/NCAR/CESM2/amip/r3i1p1f1/Amon/cl/gn/v20190319"
+c3s_cmip6 = "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon/rlds/gr/v20180803"
 
-fpath = cmip6_Cl_2
+fpath = c3s_cmip6
 filelist = glob(f"{fpath}/*.nc")
 
 output_path = f"test_data{fpath}"
