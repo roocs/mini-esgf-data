a08095b3d1db59ae3e05276fbff85e62d35883ba
Eleanor Smith <esmith88@sci4.jasmin.ac.uk>
2021-03-15 15:04:34 +0000

    delete comments

--- generate_test_data.py
+++ generate_test_data.py
@@ -4,30 +4,6 @@ import subprocess
 from glob import glob
 
 
-# generate condensed versions of data to use as test data
-
-# Amon_file_path = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas"
-# zostoga_file_path = "/badc/cmip5/data/cmip5/output1/IPSL/IPSL-CM5A-MR/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga"
-# cordex_IPSL_file_path = "/group_workspaces/jasmin2/cp4cds1/data/c3s-cordex/output/EUR-11/IPSL/MOHC-HadGEM2-ES/rcp85/r1i1p1/IPSL-WRF381P/v1/day/psl/v20190212"
-# no_time_name = "/group_workspaces/jasmin2/cp4cds1/vol1/data/c3s-cmip5/output1/ICHEC/EC-EARTH/historical/day/atmos/day/r1i1p1/tas/v20131231"
-# CMIP6_siconc = (
-#     "/badc/cmip6/data/CMIP6/CMIP/NCAR/CESM2/historical/r1i1p1f1/SImon/siconc/gn/latest"
-# )
-# cmip5_tas = "/badc/cmip5/data/cmip5/output1/NCAR/CCSM4/historical/mon/atmos/Amon/r1i1p1/latest/tas"
-# plevs_file_path = "/badc/cmip6/data/CMIP6/CMIP/NOAA-GFDL/GFDL-ESM4/historical/r1i1p1f1/Amon/o3/gr1/v20190726"
-# day_data = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp45/day/land/day/r1i1p1/latest/mrsos"
-# cmip6_Cl = "/badc/cmip6/data/CMIP6/CMIP/NCAR/CESM2/amip/r1i1p1f1/Amon/cl/gn/v20190319"
-# cmip6_Cl_2 = "/badc/cmip6/data/CMIP6/CMIP/NCAR/CESM2/amip/r3i1p1f1/Amon/cl/gn/v20190319"
-# c3s_cmip6 = "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon/rlds/gr/v20180803"
-# time_invariant = "/badc/cmip6/data/CMIP6/ScenarioMIP/IPSL/IPSL-CM6A-LR/ssp119/r1i1p1f1/fx/mrsofc/gr/v20190410"
-# cmip6_ta = "/badc/cmip6/data/CMIP6/ScenarioMIP/MIROC/MIROC6/ssp119/r1i1p1f1/Amon/ta/gn/files/d20190807"
-# C3S_CMIP5_TSICE = "/gws/nopw/j04/cp4cds1_vol1/data/c3s-cmip5/output1/NCC/NorESM1-ME/rcp60/mon/seaIce/OImon" \
-#                   "/r1i1p1/tsice/v20120614"
-# c3s_cmip6_inv_01032021_1 = "/badc/cmip6/data/CMIP6/CMIP/INM/INM-CM5-0/historical/r1i1p1f1/Amon/rlds/gr1/v20190610"
-# c3s_cmip6_inv_01032021_2 = "/badc/cmip6/data/CMIP6/ScenarioMIP/INM/INM-CM5-0/ssp245/r1i1p1f1/Amon/rlds/gr1/v20190619"
-# lat_lon_coords_only = "/badc/cmip6/data/CMIP6/CMIP/MPI-M/MPI-ESM1-2-LR/historical/r1i1p1f1/Omon/tos/gn/v20190710"
-
-
 def arg_parse():
     """
     Parses arguments given at the command line
@@ -146,10 +122,6 @@ def select_lat_lon(filelist, fpath, args):
         #if the required number of files have been generated - stop
         if args.number == n_files:
             break
-
-        # if var_id in ['o3', 'ta', 'tsice', 'tas', 'tos', 'rlds']:
-        # # Only create one file for datasets where we don't need more
-        #     break
 
 
 def main():
